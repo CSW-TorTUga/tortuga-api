67eef199c3e32effb4c5400adf4b97fbb288fa00
mischa <me@mhlz.de>
2015-11-01 16:19:18 +0100

    initial

---
+++ src/main/java/st/ilu/rms4csw/Rms4csvApiApplication.java
@@ -0,0 +1,12 @@
+package st.ilu.rms4csw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rms4csvApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rms4csvApiApplication.class, args);
+    }
+}
---
+++ src/test/java/st/ilu/rms4csw/Rms4csvApiApplicationTests.java
@@ -0,0 +1,18 @@
+package st.ilu.rms4csw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Rms4csvApiApplication.class)
+@WebAppConfiguration
+public class Rms4csvApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
