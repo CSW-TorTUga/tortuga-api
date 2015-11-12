d3e74a361ace328b191da7d36bd8e3734b1feb2d
mischa <me@mhlz.de>
2015-11-12 16:49:17 +0100

    Fixed the postUser endpoint

--- src/main/java/st/ilu/rms4csw/Main.java
+++ src/main/java/st/ilu/rms4csw/Main.java
@@ -8,6 +8,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Main {
 
+    public static String getApiBase() {
+        String envBase = System.getenv("RMS_CSW_API_BASE");
+        if(envBase != null) {
+            return envBase;
+        }
+
+        return "http://localhost:7081/api/v1";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
--- src/main/java/st/ilu/rms4csw/controller/UserController.java
+++ src/main/java/st/ilu/rms4csw/controller/UserController.java
@@ -1,8 +1,12 @@
 package st.ilu.rms4csw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import st.ilu.rms4csw.Main;
 import st.ilu.rms4csw.controller.exception.NotFoundException;
 import st.ilu.rms4csw.model.user.User;
 import st.ilu.rms4csw.repository.UserRepository;
@@ -14,9 +18,11 @@ import java.util.List;
  * @author Mischa Holz
  */
 @Controller
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/" + UserController.API_BASE)
 @ResponseBody
 public class UserController {
+
+    public final static String API_BASE = "users";
 
     private UserRepository userRepository;
 
@@ -31,8 +37,11 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    private User postUser(@RequestBody User user) {
-        return userRepository.save(user);
+    private ResponseEntity<User> postUser(@RequestBody User user, HttpHeaders headers) {
+        User ret = userRepository.save(user);
+        headers.set("Location", Main.getApiBase() + API_BASE + "/" + ret.getId());
+
+        return new ResponseEntity<>(ret, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
@@ -45,7 +54,7 @@ public class UserController {
     @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
     private User patchUser(@PathVariable String id, @RequestBody User user) {
         User original = userRepository.findOne(id);
-        if(original == null) {
+        if (original == null) {
             throw new NotFoundException("Did not find user with the id " + id);
         }
 
