4c1e2672d23ead8f5acf6b5902e312f276a94bb8
Hannes <hannes@guedelhoefer.de>
2015-11-24 14:23:02 +0100

    14396 Benutzervewaltung - added Major Repository and Controler

---
+++ src/main/java/st/ilu/rms4csw/controller/MajorController.java
@@ -0,0 +1,69 @@
+package st.ilu.rms4csw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import st.ilu.rms4csw.Main;
+import st.ilu.rms4csw.controller.exception.NotFoundException;
+import st.ilu.rms4csw.model.major.Major;
+import st.ilu.rms4csw.model.user.User;
+import st.ilu.rms4csw.repository.MajorRepository;
+import st.ilu.rms4csw.util.Patch;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Created by hannes on 24.11.15.
+ */
+@Controller
+@RequestMapping("/api/v1/" + MajorController.API_BASE)
+@ResponseBody
+public class MajorController {
+
+	public final static String API_BASE = "majors";
+
+
+	private MajorRepository majorRepository;
+
+	@Autowired
+	public MajorController(MajorRepository majorRepository) {
+		this.majorRepository = majorRepository;
+	}
+
+	@RequestMapping
+	public List<Major> findAll() {
+		return majorRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Major> postMajor(@RequestBody Major major, HttpServletResponse response) {
+		Major ret = majorRepository.save(major);
+		response.setHeader("Location", Main.getApiBase() + API_BASE + "/" + ret.getId());
+
+		return new ResponseEntity<>(ret, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public Major putMajor(@PathVariable String id, @RequestBody Major major) {
+		major.setId(id);
+
+		return majorRepository.save(major);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+	public Major patchMajor(@PathVariable String id, @RequestBody Major major) {
+		Major original = majorRepository.findOne(id);
+		if (original == null) {
+			throw new NotFoundException("Did not find Major with the id " + id);
+		}
+
+		Major patchedMajor = Patch.patch(original, major);
+
+		return majorRepository.save(patchedMajor);
+	}
+
+}
