1af882226d29748f3476a823f28b76ec74f1ede1
mischa <me@mhlz.de>
2015-11-25 17:10:30 +0100

    Fixing the semester function

--- src/main/java/st/ilu/rms4csw/model/user/User.java
+++ src/main/java/st/ilu/rms4csw/model/user/User.java
@@ -178,6 +178,11 @@ public class User extends PersistentEntity {
             calendar.set(Calendar.MONTH, Calendar.APRIL);
         }
 
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
         return calendar.getTime();
     }
 }
