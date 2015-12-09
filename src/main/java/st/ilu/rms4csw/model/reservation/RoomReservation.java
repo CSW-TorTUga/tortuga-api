40e3f2a6a66569255732106bb0cc05f17fd53e6d
mischa <me@mhlz.de>
2015-12-09 13:19:33 +0100

    Added description field to room reservations

--- src/main/java/st/ilu/rms4csw/model/reservation/RoomReservation.java
+++ src/main/java/st/ilu/rms4csw/model/reservation/RoomReservation.java
@@ -10,6 +10,16 @@ public class RoomReservation extends Reservation<RoomReservation> {
 
     private boolean approved;
 
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public boolean isApproved() {
         return approved;
     }
