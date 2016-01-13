5655aca261e71e4cf01dc37bf42d8e205f1698e2
mischa <me@mhlz.de>
2016-01-13 14:52:11 +0100

    Made parts of the login service mockable

---
+++ src/main/java/st/ilu/rms4csw/service/SpringSecurityLoggedInUserHolder.java
@@ -0,0 +1,38 @@
+package st.ilu.rms4csw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import st.ilu.rms4csw.model.user.User;
+import st.ilu.rms4csw.repository.user.UserRepository;
+import st.ilu.rms4csw.security.LoggedInUserHolder;
+
+/**
+ * @author Mischa Holz
+ */
+@Service
+public class SpringSecurityLoggedInUserHolder implements LoggedInUserHolder {
+
+    private UserRepository userRepository;
+
+    public SpringSecurityLoggedInUserHolder() {
+        System.out.println("hi!!!");
+    }
+
+    @Override
+    public User getLoggedInUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof String) {
+            return userRepository.findOneByLoginName((String) principal);
+        } else if(principal instanceof User) {
+            return (User) principal;
+        } else {
+            throw new RuntimeException("I don't know what to do with this principal: " + principal);
+        }
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
