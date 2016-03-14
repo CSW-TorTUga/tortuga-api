12b237e46513db8d3086f1492f23236cee313916
Hannes <hannes@guedelhoefer.de>
2016-03-14 16:56:51 +0100

    added local net endpoint

---
+++ src/main/java/st/ilu/rms4csw/controller/api/LocalNetController.java
@@ -0,0 +1,25 @@
+package st.ilu.rms4csw.controller.api;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import st.ilu.rms4csw.util.NetworkUtil;
+
+/**
+ * Created by hannes on 14.03.16.
+ */
+@Controller
+@RequestMapping("/localnet")
+public class LocalNetController {
+
+
+    @RequestMapping()
+    public ResponseEntity<Void> localNet() {
+        if(NetworkUtil.isLocalNetworkRequest()) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+}
--- src/main/java/st/ilu/rms4csw/controller/api/terminal/TerminalController.java
+++ src/main/java/st/ilu/rms4csw/controller/api/terminal/TerminalController.java
@@ -3,6 +3,7 @@ package st.ilu.rms4csw.controller.api.terminal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
