14da24cd00559ee9acd022741d1664edfc434a6e
mischa <me@mhlz.de>
2016-03-16 15:36:43 +0100

    Added a test for the localnet controller

---
+++ src/test/java/st/ilu/rms4csw/controller/LocalNetControllerTest.java
@@ -0,0 +1,47 @@
+package st.ilu.rms4csw.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import st.ilu.rms4csw.TestContext;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Mischa Holz
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestContext.class)
+@WebAppConfiguration
+public class LocalNetControllerTest {
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testLocalNet() throws Exception {
+        mockMvc.perform(get("/api/v1/localnet")).andExpect(status().isOk());
+    }
+}
