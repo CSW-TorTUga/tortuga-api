77e6e448241b9f373cacb564ad594111ef706880
mischa <me@mhlz.de>
2016-03-16 15:38:30 +0100

    Added DELETED Role

--- src/main/java/st/ilu/rms4csw/model/user/Role.java
+++ src/main/java/st/ilu/rms4csw/model/user/Role.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 public enum Role {
 
 
+    DELETED,
     STUDENT("OP_STUDENT"),
     LECTURER("OP_STUDENT", "OP_LECTURER"),
     CSW_TEAM("OP_STUDENT", "OP_LECTURER", "OP_TEAM"),
