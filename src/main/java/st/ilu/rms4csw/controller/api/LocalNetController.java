b212e8276d77776491d38c083525ed0f8216ad9c
mischa <me@mhlz.de>
2016-03-16 10:35:22 +0100

    Fixed the local net controller

--- src/main/java/st/ilu/rms4csw/controller/api/LocalNetController.java
+++ src/main/java/st/ilu/rms4csw/controller/api/LocalNetController.java
@@ -1,24 +1,19 @@
 package st.ilu.rms4csw.controller.api;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import st.ilu.rms4csw.util.NetworkUtil;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
- * Created by hannes on 14.03.16.
+ * @author Hannes Güdelhöfer
  */
-@Controller
+@RestController
 @RequestMapping("/api/v1/localnet")
 public class LocalNetController {
-
-    @RequestMapping()
-    public ResponseEntity<String> localNet() {
-        if(NetworkUtil.isLocalNetworkRequest()) {
-            return new ResponseEntity<String>("true", HttpStatus.OK);
-        }
-        return new ResponseEntity<String>("false", HttpStatus.OK);
+    @RequestMapping
+    public Boolean localNet(HttpServletRequest request) {
+        return NetworkUtil.isLocalNetworkRequest(request);
     }
 }
