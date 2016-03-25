82b2bbab0ab8939a6e17965674e551c1628047ed
mischa <me@mhlz.de>
2016-03-25 12:15:22 +0100

    Added the dumbest test of all time

--- src/test/java/de/computerstudienwerkstatt/tortuga/controller/DoorAuthorisationAttemptControllerTest.java
+++ src/test/java/de/computerstudienwerkstatt/tortuga/controller/DoorAuthorisationAttemptControllerTest.java
@@ -1,9 +1,11 @@
 package de.computerstudienwerkstatt.tortuga.controller;
 
 import de.computerstudienwerkstatt.tortuga.TestContext;
+import de.computerstudienwerkstatt.tortuga.controller.api.statistics.DoorAuthorisationAttemptController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,7 +15,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Method;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,6 +34,9 @@ public class DoorAuthorisationAttemptControllerTest {
 
     private MockMvc mockMvc;
 
+    @Autowired
+    private DoorAuthorisationAttemptController doorAuthorisationAttemptController;
+
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -39,5 +46,15 @@ public class DoorAuthorisationAttemptControllerTest {
     public void testFindAll() throws Exception {
         mockMvc.perform(get("/api/v1/stats/doorauthorisationattempts").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testApiBase() throws Exception {
+        Method method = DoorAuthorisationAttemptController.class.getDeclaredMethod("getApiBase");
+        method.setAccessible(true);
+        Object ret = method.invoke(doorAuthorisationAttemptController);
+
+        assertTrue(ret instanceof String);
+        assertTrue(ret.toString().equals(DoorAuthorisationAttemptController.API_BASE));
     }
 }
